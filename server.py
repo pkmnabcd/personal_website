1c7adacb4452550aa1d3fc81773c8a033e25bfe1
Gabe Decker <42gdeckers@gmail.com>
2026-05-30 23:59:29 -0600

    I realized that the new request handler just implemented the default behavior of the class it was subclassing lol

--- server.py
+++ server.py
@@ -2,18 +2,9 @@ import os
 from sys import exit
 from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
 
-class MyWebsiteHandler(SimpleHTTPRequestHandler):
-    # This overrides the default behavior only for the root URL "/"
-    # The file will be found based on the uri path
-    def do_GET(self):
-        if self.path == "/":
-            self.path = "/index.html"
-
-        # Python handles the reading, opening, and serving the files safely.
-        return super().do_GET()
 
 if __name__ == "__main__":
-    # Ensure the server runs out of the directory where this script lives
+    # Make the working directory the 'website' dir
     web_dir = os.path.dirname(__file__)
     public_dir = os.path.join(web_dir, "website")
     if os.path.exists(public_dir):
@@ -22,6 +13,8 @@ if __name__ == "__main__":
         print(f"The intended working dir {public_dir} not found. Exiting.")
         exit()
 
-    server = ThreadingHTTPServer(("127.0.0.1", 8000), MyWebsiteHandler)
+    # See the following documentation for SimpleHTTPRequestHandler behavior.
+    # https://docs.python.org/3/library/http.server.html#http.server.SimpleHTTPRequestHandler.do_GET
+    server = ThreadingHTTPServer(("127.0.0.1", 8000), SimpleHTTPRequestHandler)
     print("Server running on http://127.0.0.1:8000 ...")
     server.serve_forever()
