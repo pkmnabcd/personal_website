37f44e7bd34956cdb6dd9d6e9f9ccc9a6ac931b6
Gabe Decker <42gdeckers@gmail.com>
2026-05-30 23:38:18 -0600

    Updating server slightly

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import os
+from sys import exit
 from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
 
 class MyWebsiteHandler(SimpleHTTPRequestHandler):
     # This overrides the default behavior only for the root URL "/"
+    # The file will be found based on the uri path
     def do_GET(self):
         if self.path == "/":
             self.path = "/index.html"
@@ -13,8 +15,12 @@ class MyWebsiteHandler(SimpleHTTPRequestHandler):
 if __name__ == "__main__":
     # Ensure the server runs out of the directory where this script lives
     web_dir = os.path.dirname(__file__)
-    if web_dir:
-        os.chdir(web_dir)
+    public_dir = os.path.join(web_dir, "website")
+    if os.path.exists(public_dir):
+        os.chdir(public_dir)
+    else:
+        print(f"The intended working dir {public_dir} not found. Exiting.")
+        exit()
 
     server = ThreadingHTTPServer(("127.0.0.1", 8000), MyWebsiteHandler)
     print("Server running on http://127.0.0.1:8000 ...")
